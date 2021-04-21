b9300860925fdd066d6212da53b74a0ac4304949
Haley Whitman <53501096+camelops@users.noreply.github.com>
2021-04-21 16:01:30 -0400

    part4a blog unittest step2

--- part4/blog/server/utils/list_helper.js
+++ part4/blog/server/utils/list_helper.js
@@ -1,8 +1,19 @@
 // eslint-disable-next-line no-unused-vars
-const dummy = (_blogs) => {
+const dummy = (blogs) => {
   return 1
+}
+
+const totalLikes = (blogs) => {
+  let total = 0
+
+  blogs.forEach(function(blog) {
+    total += blog.likes
+  })
+
+  return total
 }
   
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
