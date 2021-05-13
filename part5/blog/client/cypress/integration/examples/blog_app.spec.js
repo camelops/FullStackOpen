4851082037a4976d714e03bf1f67a84254d3798a
Haley Whitman <53501096+camelops@users.noreply.github.com>
2021-05-12 20:35:49 -0400

    part5d blog e2e test step2

--- part5/blog/client/cypress/integration/examples/blog_app.spec.js
+++ part5/blog/client/cypress/integration/examples/blog_app.spec.js
@@ -1,7 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    cy.visit('http://localhost:3000')
+    cy.request('POST', 'http://localhost:3003/api/users', {
+      username: 'mluukkai', password: 'salainen'
+    }).then(response => {
+      localStorage.setItem('loggedNoteappUser', JSON.stringify(response.body))
+      cy.visit('http://localhost:3000')
+    })
   })
 
   it('login form is shown', function() {
@@ -9,13 +14,22 @@ describe('Blog app', function() {
     cy.contains('password')
   })
 
-  it('Login form can be opened', function() {
-    cy.contains('login').click()
-    cy.get('#username').type('test')
-    cy.get('#password').type('test123')
-    cy.get('#login-button').click()
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.contains('login').click()
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+      cy.contains('mluukkai has successfully logged in')
+    })
 
-    cy.contains('Test User is logged in')
-
+    it('fails with wrong credentials', function() {
+      cy.contains('login').click()
+      cy.get('#username').type('test')
+      cy.get('#password').type('test123')
+      cy.get('#login-button').click()
+      cy.contains('Wrong credentials entered')
+    })
   })
+
 })
