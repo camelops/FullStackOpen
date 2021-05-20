b02fae8b7f27863010d72fa350b47251fd4a91bf
Haley Whitman <53501096+camelops@users.noreply.github.com>
2021-05-20 14:22:31 -0400

    part6b anecdotes step7

--- part6/lecturenotes/src/index.js
+++ part6/lecturenotes/src/index.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
 
-const store = createStore(noteReducer)
+import { createNote } from './reducers/noteReducer'
+import { filterChange } from './reducers/filterReducer'
+
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+
+const store = createStore(
+  reducer,
+  composeWithDevTools()
+  )
+
+console.log(store.getState())
+
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 ReactDOM.render(
   <Provider store={store}>
@@ -13,3 +32,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
+
+export default store
