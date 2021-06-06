18fd8f90f4c0ab0c56ec65e13105c9a29ce1f93c
Haley Whitman <53501096+camelops@users.noreply.github.com>
2021-06-05 23:30:28 -0400

    part7b hooked anecdotes step2

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -79,6 +79,13 @@ const CreateNew = (props) => {
     props.history.push('/')
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -96,6 +103,7 @@ const CreateNew = (props) => {
           <input {...info}/>
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
